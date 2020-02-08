c2552d4ef345053e6cf55c389b9633c70f4a4127
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-02-08 10:00:27 -0500

    created multiple functions

--- script.js
+++ script.js
@@ -1,106 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var randomNumber = ['0','1','2','3','4','5','6','7','8','9'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var symbol = ['~','!','@','#','$','%','^','&','*','(',')','-'];
 
-var password = prompt("How long would you like your password? Please enter a number 8 -128.")
 
-//BELOW: OR would it be 'for' --> 'alert' --> 'if' ??
-var lowerCase = alert("Would you like your password to contain lower case letters? Click 'OK' for yes or 'Cancel' for no.");
+// Write password to the #password input
+function writePassword(password) {    //function that writes the password to the DOM
+  
+  var passwordText = document.querySelector("#password");   //grabs the area with the ID of #password
 
-if(lowerCase === true) {
-  for (var i = 0; i < lowerCase.length; i++) {
-    
-    console.log(lowerCase[i]);
+  passwordText.value = password;  //writes the password to the DOM
+}
+function generatePassword(length, arrChar) {  //taking the array of characters that they have selected and the length and generating the password from what they have selected
+  var passwordHolder = ""
+  console.log(length, arrChar)
+  for (var i = 0; i < length; i++) {
+    var randomArr = Math.floor(Math.random()*arrChar.length)
+    var randomChar = Math.floor(Math.random()*arrChar[randomArr].length)
+    passwordHolder= passwordHolder + arrChar[randomArr][randomChar]
+    console.log(passwordHolder)
+
+  }
+  writePassword(passwordHolder) //calling what has already been established
+}
+
+function characters(length) {
+  var arrChar =[]
+  //lower case letters
+
+  if(confirm("Would you like your password to contain lower case letters? Click 'OK' for yes or 'Cancel' for no.")) {
+      arrChar.push(lowerCase);
+  };
+   //upper case letters
+  
+  if(confirm("Would you like your password to contain upper case letters? Click 'OK' for yes or 'Cancel' for no.")) {
+  // For Loop for upperCase
+    arrChar.push(upperCase)
+  }
+  
+  //numbers
+
+  
+  if(confirm("Would you like your password to contain numbers? Click 'OK' for yes or 'Cancel' for no.")) {
+  // For Loop for randomNumber
+    arrChar.push(numbers)
+  }
+  
+  //symbols
+
+  
+  if(confirm("Would you like your password to contain numbers? Click 'OK' for yes or 'Cancel' for no.")) {
+  // For Loop for symbol
+    arrChar.push(symbol)
+  }
+  if(arrChar.length){
+    generatePassword(length, arrChar)
+  }
+  else{
+    alert("You must select at least one character type")
+    characters(length)
   }
 }
 
-var upperCase = alert("Would you like your password to contain upper case letters? Click 'OK' for yes or 'Cancel' for no.")
+function start() {
 
-if(upperCase === true) {
-// For Loop for upperCase
-for (var j = 0; j < upperCase.length; j++) {
-  console.log(upperCase[j]);
+  var pwLength = prompt("How long would you like your password? Please enter a number 8 - 128.");
+  if (pwLength < 8 || pwLength > 128) {
+    pwLength = alert("Your password must be between 8 and 128 characters.");
+    start()
+  } else{
+    characters(pwLength)
   }
 }
-
-
-var randomNumber = alert("Would you like your password to contain numbers? Click 'OK' for yes or 'Cancel' for no.")
-
-if(randomNumber ==== true) {
-// For Loop for randomNumber
-for (var k = 0; k < randomNumber.length; k++) {
-  console.log(randomNumber[k]);
-  }
-}
-
-var symbol = alert("Would you like your password to contain numbers? Click 'OK' for yes or 'Cancel' for no.")
-
-if(symbol === true) {
-// For Loop for symbol
-for (var l = 0; l < symbol.length; l++) {
-  console.log(symbol[l]);
-  }
-}
+//length of password
 
 
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", start);
 
 
-
-//----------------------------------------------------
-
-//From one video
-//possible password values
-var values = /^[0-9a-zA-Z]+$/; //OR let values = "write them"
-
-let possiblepassword = "";
-
-//create for loop to choose pw characters
-for(var i = 0; i < passwordText; i++) {
-    possiblepassword = possiblepassword + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
-
-//add password to display area
-document.getElementById("password").value = possiblepassword;
-
-//-----------------------------------------------------------
-
-//From video:
-function lowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function UpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function symbol() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-
-
-
-console.log(lowerCase());
-console.log(UpperCase());
-console.log(randomNumber());
-console.log(symbol());
 
